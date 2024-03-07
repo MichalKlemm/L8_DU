42032f83bacefe36da202fb55c0a5df5c46cb1e3
MichalKlemm <Klemmmichal@lgmail.com>
2024-03-07 21:29:55 +0100

    Vytvoření čtečky
    Spring boot s 3 endpointy (GET)
    načtení souborů a jejich vypsání do prohlížeče

--- L8_DU/src/main/java/cz/engeto/L8_DU/L8DuApplication.java
+++ L8_DU/src/main/java/cz/engeto/L8_DU/L8DuApplication.java
@@ -1,5 +1,4 @@
 package cz.engeto.L8_DU;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- L8_DU/src/main/java/cz/engeto/L8_DU/RSSReader/RSSReader.java
+++ L8_DU/src/main/java/cz/engeto/L8_DU/RSSReader/RSSReader.java
@@ -3,7 +3,6 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
